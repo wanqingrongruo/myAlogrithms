067adbf005b635183ada9e87d010b94fa2fa78da
婉卿容若 <devzheng@qq.com>
2017-02-13 17:58:06 +0800

    binarySearchTree

--- BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree.playground/Contents.swift
@@ -5,7 +5,7 @@ import UIKit
 
 var str = "Hello, i am binary search tree"
 
-enum BinaryTree<T>{
+enum BinaryTree<T: Comparable>{
     case empty
     indirect case node(BinaryTree, T, BinaryTree) // inderect 间接
 //    间接
@@ -18,6 +18,111 @@ enum BinaryTree<T>{
 //    这就是 indirect（间接）这个关键字的由来。indirect（间接）实现了一个两个类型值之间的 indirection（间接层）。这引出了语义与类型值之间的一层中间层。
 //    
 //    这个枚举现在引用的是它的关联值而不是自身的值。引用值有着一个确切的大小，所以就不再存在之前的问题。
+    
+    
+    /// insert new node
+    ///
+    /// - Parameter newValue: new value
+    // mutating : 可变的意思 -- 创建了尝试在类型值(这里就是 BinaryTree 这个枚举)中改变什么东西的方法
+    // 问题: 每次你尝试着去修改这棵树的时候，一个新的子树的拷贝就会被创建。这个新的拷贝是不会链接到你的旧的拷贝的，所以你的最开始的二叉树是永远不会被新的值所修改的。
+    mutating func naiveInsert(newValue: T){
+        
+        // 没有结点
+        // slef is BinaryTree
+        guard case .node(var left, let value, var right) = self else {
+            self = .node(.empty, newValue, .empty)
+            return
+        }
+        
+        //
+        if newValue < value {
+           left.naiveInsert(newValue: newValue)
+        }else{
+            right.naiveInsert(newValue: newValue)
+        }
+    }
+    
+    
+    private func newTreeWithInsertedValue(newValue: T) -> BinaryTree{
+        switch self {
+        case .empty:
+            return .node(.empty, newValue, .empty)
+            
+        case let .node(left, value, right):
+            if newValue < value {
+                return .node(left.newTreeWithInsertedValue(newValue: newValue), value, right)
+            }else{
+                return .node(left, value, right.newTreeWithInsertedValue(newValue: newValue))
+            }
+        }
+    }
+    
+    mutating func insert(newValue: T){
+        self = newTreeWithInsertedValue(newValue: newValue)
+    }
+    
+    // 中序遍历
+    // 中序遍历是按照升序来遍历一颗二叉搜索树的。
+    func traverseInOrder(process: (T) -> ()){
+        switch self {
+        case .empty:
+            return
+        case let .node(left, value, right):
+            left.traverseInOrder(process: process)
+            process(value)
+            right.traverseInOrder(process: process)
+
+        }
+    }
+    
+    // 先序遍历
+    // 一种在遍历过程中首先遍历节点的遍历方法
+    // 这里的关键是在遍历子树之前首先调用 process 方法
+    func traversePreOrder(process: (T) -> ()){
+        switch self {
+        case .empty:
+            return
+        case let .node(left, value, right):
+           
+            process(value)
+            left.traverseInOrder(process: process)
+            right.traverseInOrder(process: process)
+            
+        }
+
+    }
+    
+    // 后序遍历
+    // 首先遍历左子树和右子树的遍历方法
+    func traversePostOrder(process: (T) -> ()){
+        switch self {
+        case .empty:
+            return
+        case let .node(left, value, right):
+            
+            left.traverseInOrder(process: process)
+            right.traverseInOrder(process: process)
+            process(value)
+        }
+    }
+    
+    // 搜索
+    func search(searchValue: T) -> BinaryTree?{
+        switch self {
+        case .empty:
+            return nil
+        case let .node(left, value, right):
+            if searchValue == value {
+                return self
+            }
+            if searchValue < value {
+                return left.search(searchValue: searchValue)
+            }else{
+                return right.search(searchValue: searchValue)
+            }
+      
+        }
+    }
 }
 
 // left node
@@ -70,4 +175,26 @@ tree.count
 
 // Binary search tree
 // 二叉搜索树的主要特性: 每个左子树的数值小于它的父结点的数值，每个右子树的数值大于父结点的数值
+
+var binaryTree: BinaryTree<Int> = .empty
+
+//binaryTree.naiveInsert(newValue: 5) // binaryTree now has a node value with 5
+//binaryTree.naiveInsert(newValue: 7) // binaryTree is unchanged -- 原树没有被改变
+//binaryTree.naiveInsert(newValue: 9)
+
+binaryTree.insert(newValue: 5)
+binaryTree.insert(newValue: 7)
+binaryTree.insert(newValue: 9)
+
+binaryTree.traverseInOrder { print($0) }
+
+print("***************")
+binaryTree.traversePreOrder { print($0) }
+
+print("***************")
+binaryTree.traversePostOrder { print($0) }
+
+print("***************")
+
+binaryTree.search(searchValue: 7)
 
